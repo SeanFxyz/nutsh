b1bb8ac36de328f022894c497331b24d44c4d39b
Sean <sean_m_fitzgerald@tutanota.com>
2021-08-31 16:14:20 -0700

    Commented nsh_tokenize in nutsh.h

--- nutsh.h
+++ nutsh.h
@@ -61,34 +61,78 @@ char *nsh_readline()
     return buffer;
 }
 
+// Expects a line of text, breaks it up into tokens, and returns an array of
+// token strings.
+// Tokens are separated by unescaped spaces.
+//
+// Originally, this simply used the standard library's 'strtok' to break the
+// line into tokens, but this meant that tokens could not escape spaces.
+//
+// Now, it uses strtok break off the first char sequence with no spaces, then
+// copies the sequence to a separate buffer at tok_copy. If the sequence ends
+// in a backslash, this indicates the space between this sequence and the next
+// was meant to be escaped, so the next token will be merged with the current
+// token, and this is repeated until the current sequence no longer ends with
+// a backslash. The tok_copy pointer's current address is then pushed onto a
+// dynamic array. This process in repeated until the line has been processed
+// and each token has been pushed into the dynamic array. The dynamic array is
+// then terminated with a NULL sentinel value and contracted to free unused
+// memory, and a normal char** tokens_p is assigned to the dynamic array's data
+// addres and returned.
 char **nsh_tokenize(char *line)
 {
+    // Dynamic array to store tokens.
     darr *tokens = darr_create(sizeof(char*), 64);
-    char **tokens_p;
-    size_t tok_len;
-    char *token;
-    char *tok_copy;
 
+    char **tokens_p; // Will be set to address of final char** and returned.
+    size_t tok_len;  // To keep track of the length of the current token.
+    char *token;     // Keeps track of the current token
+                     //   (this is an address within the original 'line' string)
+    char *tok_copy;  // Copy of 'token' that will be added to 'tokens'
+
+    // Get a pointer to the first space-delimited sequence within line
     token = strtok(line, NSH_TOK_DELIM);
+
+    // While we have a sequence to process...
     while (token != NULL) {
+
+        // Get the token length
         tok_len = strlen(token);
 
+        // Allocate a buffer to make a copy of the sequence
         tok_copy = malloc(tok_len);
-        check_mem(tok_copy);
+        check_mem(tok_copy); // check allocation
+
+        // Copy sequence to the new buffer.
         strcpy(tok_copy, token);
 
+        // While the copy's last character is a backslash...
         while (tok_copy[tok_len - 1] == '\\') {
+            // Replace the backslash with a space
             tok_copy[tok_len - 1] = ' ';
+
+            // Get the next space-delimited sequence
+            // (NULL for the first argument means strtok will get the next tok in
+            // the last string given)
             token = strtok(NULL, NSH_TOK_DELIM);
+
+            // Add the new 'token' sequence's length the the total
             tok_len += strlen(token);
 
+            // Grow the buffer at 'tok_copy' to accomodate the new sequence
             tok_copy = realloc(tok_copy, tok_len);
-            check_mem(tok_copy);
+            check_mem(tok_copy); // check allocation
+
+            // Add the new sequence to the end of tok_copy
             strcat(tok_copy, token);
         }
 
+        // Add tok_copy's current value to the end of the dynamic array.
         darr_push(tokens, tok_copy);
 
+        // Get the next space-delimited sequence
+        // (NULL for the first argument means strtok will get the next tok in
+        // the last string given)
         token = strtok(NULL, NSH_TOK_DELIM);
     }
 
